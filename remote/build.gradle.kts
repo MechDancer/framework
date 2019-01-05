7814e196fc5b63cc2dd12468496b77055044033f
YangDR <ydrml@hotmail.com>
2019-01-05 20:15:50 +0800

    测试长连接
    Signed-off-by: YangDR <ydrml@hotmail.com>

--- remote/build.gradle.kts
+++ remote/build.gradle.kts
@@ -5,7 +5,7 @@ apply {
     plugin("com.novoda.bintray-release")
 }
 
-version = "0.2.1-dev-7"
+version = "0.2.1-dev-8"
 
 dependencies {
     compile(kotlin("stdlib"))
---
+++ remote/src/test/kotlin/org/mechdancer/remote/longConnection.kt
@@ -0,0 +1,54 @@
+package org.mechdancer.remote
+
+import org.mechdancer.remote.modules.tcpconnection.listenString
+import org.mechdancer.remote.modules.tcpconnection.say
+import org.mechdancer.remote.presets.RemoteDsl.Companion.remoteHub
+import kotlin.concurrent.thread
+
+private object Server2 {
+    val name = Server2::class.java.simpleName!!
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val hub = remoteHub(name) {
+            newMemberDetected { println("- detected $it") }
+        }
+        thread(isDaemon = true) { while (true) hub() }
+        thread(isDaemon = true) { while (true) hub.accept() }
+        while (hub[Client2.name]?.connected != true);
+        hub.processConnection(Client2.name) {
+            println("- connected")
+            while (true) {
+                val msg = it.listenString()
+                println("- hear $msg")
+                if (msg == "over") break // 在商定的条件出现时退出
+                it.say("server heard $msg")
+            }
+        }
+    }
+}
+
+private object Client2 {
+    val name = Client2::class.java.simpleName!!
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val hub = remoteHub(name) {
+            newMemberDetected {
+                println("- detected $it")
+            }
+        }
+        hub.openFirst { "VM" !in it.name }
+        thread(isDaemon = true) { while (true) hub() }
+        while (!hub.connectKeeping(Server2.name));
+        hub.processConnection(Server2.name) {
+            println("- connected")
+            while (true) {
+                val msg = readLine()!!
+                it.say(msg)
+                if (msg == "over") break
+                println(it.listenString())
+            }
+        }
+    }
+}
